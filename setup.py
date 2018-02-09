5344d3ff4195633e12eeb5adbdeaf036aa9a7afe
Omar Elazhary <omazhary@gmail.com>
2018-02-08 21:47:46 -0800

    Updates directory structure

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(
-        name='git-sherlock',
+        name='gitsherlock',
         version='0.1',
         description='A GitHub mining tool for research purposes.',
         url='https://github.com/omazhary/git-sherlock',
         author='omazhary',
         author_email='omazhary@gmail.com',
         license='MIT',
-        packages=['git-sherlock'],
-        scripts=['git-sherlock'],
+        packages=['gitsherlock'],
         zip_safe=False
 )
